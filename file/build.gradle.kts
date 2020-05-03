55b8e089fa0e640550f19449b67f591a78cbf1e6
Young hoon Go <kyhsa93@naver.com>
2020-05-03 16:10:30 +0900

    Add swagger2

--- file/build.gradle.kts
+++ file/build.gradle.kts
@@ -17,6 +17,8 @@ repositories {
 }
 
 dependencies {
+	compile ("io.springfox:springfox-swagger2:2.9.2")
+	compile ("io.springfox:springfox-swagger-ui:2.9.2")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
--- file/src/main/kotlin/com/example/file/DemoApplication.kt
+++ file/src/main/kotlin/com/example/file/DemoApplication.kt
@@ -2,8 +2,10 @@ package com.example.file
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 @SpringBootApplication
+@EnableSwagger2
 class DemoApplication
 
 fun main(args: Array<String>) {
