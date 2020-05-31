a516b2522472bdce911a46da5343c1e00d19e263
Young hoon Go <kyhsa93@naver.com>
2020-05-31 12:58:47 +0900

    Add UserCreatedEventHandler spec

--- user/src/users/event/created.handler.ts
+++ user/src/users/event/created.handler.ts
@@ -3,20 +3,20 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Inject } from '@nestjs/common';
 
-import Event from '@src/users/entity/event';
+import EventEntity from '@src/users/entity/event';
 import UserCreatedEvent from '@src/users/event/created';
 import Producer from '@src/users/message/producer';
 
 @EventsHandler(UserCreatedEvent)
 export default class UserCreatedEventHandler implements IEventHandler {
   constructor(
-    @InjectRepository(Event) private readonly eventRepository: Repository<Event>,
+    @InjectRepository(EventEntity) private readonly eventRepository: Repository<EventEntity>,
     @Inject(Producer) private readonly messageProducer: Producer,
   ) {}
 
   public async handle(event: UserCreatedEvent): Promise<void> {
     const { id, userId, type } = event;
-    const eventEntity = new Event(id, userId, type);
+    const eventEntity = new EventEntity(id, userId, type);
     this.messageProducer.sendToQueue('test');
     await this.eventRepository.save(eventEntity);
   }
