e469771feb9504b0b962cd288e2538abbb204d7b
Young hoon Go <kyhsa93@naver.com>
2020-06-29 22:33:02 +0900

    Add service config

--- user/src/users/service/profile.ts
+++ user/src/users/service/profile.ts
@@ -1,20 +1,22 @@
 import { Injectable, HttpService, InternalServerErrorException } from '@nestjs/common';
 
+import AppConfiguration from '@src/app.config';
+
 interface Profile {
   userId: string;
 }
 
 @Injectable()
 export default class ProfileService {
+  private readonly config = AppConfiguration;
+
   constructor(private readonly httpService: HttpService) {}
 
   public async findByUserId(userId: string): Promise<Profile[]> {
-    const observableResponse = this.httpService.get(
-      `http://localhost:7000/profiles?userId=${userId}`,
-    );
+    const url = `${this.config.service.profile}?userId=${userId}`;
+    const observableResponse = this.httpService.get(url);
     const response = await observableResponse.toPromise();
-    if (!response || !response.data)
-      throw new InternalServerErrorException('profile service is not available');
+    if (!response || !response.data) throw new InternalServerErrorException('profile service is not available');
     return response.data as Profile[];
   }
 }
