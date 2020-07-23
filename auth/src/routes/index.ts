0529cfe6b197949898c47d3f42ef6b431e6080d7
simson613 <53423186+simson613@users.noreply.github.com>
2020-07-23 13:28:57 +0900

    Delete dummy in auth service (#65)

--- auth/src/routes/index.ts
+++ auth/src/routes/index.ts
@@ -1,10 +1,8 @@
 import { Router } from 'express';
-import auth from './auth';
 import user from './user';
 
 const routes = Router();
 
-// routes.use('/auth', auth);
 routes.use('/user', user);
 
 export default routes;
